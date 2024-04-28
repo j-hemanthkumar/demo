9526c85f769846be6b5780747d3fefd4b1ce1bb6
j-hemanthkumar <165656778+j-hemanthkumar@users.noreply.github.com>
2024-04-28 18:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 def get_image_url():
     # Authenticate with Azure Key Vault
     credential = DefaultAzureCredential()
-    key_vault_uri = "YOUR_KEY_VAULT_URI"
+    key_vault_uri = "c219cf67-c90d-48ff-a0fc-36b560fe855b"
     # Retrieve the connection string secret from Azure Key Vault
     blob_storage_connection_string = ...
  
     # Connect to Blob Storage
     blob_service_client = BlobServiceClient.from_connection_string(blob_storage_connection_string)
-    blob_client = blob_service_client.get_blob_client(container="YOUR_CONTAINER_NAME", blob="YOUR_BLOB_NAME")
+    blob_client = blob_service_client.get_blob_client(container="demoimage", blob="1705549725820.jpg")
  
     # Get the image URL
     image_url = blob_client.url
-    return image_url
+    return render_template("main.html", image_url=image_url)
  
 # Route to display the image in the web browser
 @app.route("/")
